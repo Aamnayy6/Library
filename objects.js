2f09e784be74b043abc01ba56fe383c819a285e1
Aamnayy6 <59198246+Aamnayy6@users.noreply.github.com>
2021-07-28 01:50:05 +0300

    update

--- objects.js
+++ objects.js
@@ -1,21 +1,31 @@
 let Library=[];
+let readcount=0;
 const addbutton = document.getElementById('addbutton');
 const newbook=document.getElementById('newbook');
+const bookform = document.getElementById('bookform');
 function Book(title, author, pages, read){
     this.title=title;
     this.author=author;
     this.pages=pages;
-    if(read=="Read")
-    this.read="Read";
+    if(read=="Read"){
+        this.read="Read";
+        readcount++;
+    }
     else
-    this.read = "Not read yet"
+    this.read = "Not read yet";
     this.info= function(){
         let str =  this.title + "<br/>Author: " + this.author + "<br/>Number of Pages: " + this.pages +"<br/>Read Status: " + this.read + "<br/>";
         return str;
     }
 }
+function updatebooklog(){
+    const logs = document.querySelectorAll('.log');
+    logs[0].textContent = "Total Books "+Library.length;
+    logs[1].textContent = "Books Read "+readcount;
+    logs[2].textContent ="Books Unread "+ (Library.length-readcount);
+}
 function addBookToLibrary(){
-    const bookform = document.getElementById('bookform');
+  
     let title=bookform.elements[0].value;
     let author = bookform.elements[1].value;
     let pages = bookform.elements[2].value;
@@ -28,16 +38,23 @@ function addBookToLibrary(){
     Library.push(creatingbook);
     newbook.style.display="none";
     displaybooks(creatingbook);
+    updatebooklog();
 }
-addbutton.addEventListener('click', addBookToLibrary);
+addbutton.addEventListener('click', ()=>{
+    if(bookform.checkValidity())
+    addBookToLibrary();
+});
 
 function displaybooks(creatingbook){
         let bookssection = document.querySelector('.books');
         let newbook = document.createElement('div');
         let removebook = document.createElement('button');
+        let para = document.createElement('p');
+        para.classList.add('paraedits');
         removebook.classList.add('bookdisplaybuttons');
         removebook.innerHTML = "Remove Book";
-        newbook.innerHTML += creatingbook.info();
+        newbook.appendChild(para);
+        para.innerHTML += creatingbook.info();
         newbook.classList.add('bookdisplay');
         bookssection.appendChild(newbook);
         newbook.appendChild(removebook);
@@ -56,13 +73,18 @@ function displaybooks(creatingbook){
         newbook.appendChild(readstatus);
         readstatus.addEventListener('click', ()=>{
             if(readstatus.innerHTML=="Read"){
+                readcount++;
             readstatus.innerHTML = "Unread";
             creatingbook.read="Read";
+            updatebooklog();
         }
             else{
+                readcount--;
             readstatus.innerHTML = "Read";
             creatingbook.read="Not read yet";
+            updatebooklog();
             }
+            para.innerHTML = creatingbook.info(); 
         })
     
 }
